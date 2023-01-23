bf0692e1d59cad21db387c389eae83893e5b736b
mannyaalonso <mannyaalonso@gmail.com>
2023-01-23 09:09:43 -0500

    added submarine class

---
+++ Ships/Submarine.js
@@ -0,0 +1,39 @@
+export class Submarine {
+  constructor() {
+    this.name = "Submarine"
+    this.health = 3
+  }
+
+  hit() {
+    this.health -= 1
+  }
+
+  place(letters, numbers) {
+    const direction = Math.floor(Math.random() * 2)
+    if (direction === 0) return (this.position = placeVertically(letters))
+    if (direction === 1)
+      return (this.position = placeHorizontally(letters, numbers))
+  }
+}
+
+function placeVertically(letters) {
+  const verticalLetter = Math.floor(Math.random() * 8)
+  const veritcalNumber = Math.floor(Math.random() * 10) + 1
+  const verticalPosition = [
+    `${letters[verticalLetter]}${veritcalNumber}`,
+    `${letters[verticalLetter + 1]}${veritcalNumber}`,
+    `${letters[verticalLetter + 2]}${veritcalNumber}`,
+  ]
+  return verticalPosition
+}
+
+function placeHorizontally(letters, numbers) {
+  const horizontalLetter = Math.floor(Math.random() * 8)
+  const horizontalNumber = Math.floor(Math.random() * 8)
+  const horizontalPosition = [
+    `${letters[horizontalLetter]}${numbers[horizontalNumber]}`,
+    `${letters[horizontalLetter]}${numbers[horizontalNumber + 1]}`,
+    `${letters[horizontalLetter]}${numbers[horizontalNumber + 2]}`,
+  ]
+  return horizontalPosition
+}
